16d170b3341ad81bdb3e078cb55ca9c6525167e3
miromarchi <miro.marchi@gmail.com>
2014-01-22 22:25:22 +0100

    added rbv_kala_default kalatheme subtheme

---
+++ rbv_kala_default/template.php
@@ -0,0 +1,13 @@
+<?php 
+/** 
+ * @file 
+ * Primary pre/preprocess functions and alterations.
+ */
+
+/**
+ * function rbv_kala_default_preprocess_node(&$vars) {
+ *   if ($vars['node']->type == 'org' && $vars['view_mode'] == 'default_full') {
+ *     $vars['theme_hook_suggestions'][] = 'node__org__default_full';
+ *   }
+ * }
+ */
