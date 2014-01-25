7dba89aa8f1a631d2f3d75c4a0b7ac219ea795e1
miromarchi <miro.marchi@gmail.com>
2014-01-25 11:07:33 +0100

    more css

--- rbv_kala_default/template.php
+++ rbv_kala_default/template.php
@@ -8,10 +8,12 @@
   if ($vars['node']->type == 'org' && $vars['view_mode'] == 'vmode_group_content') {
     $vars['theme_hook_suggestions'][] = 'node__org__vmode_group_content';
   }
-} */
+} 
 
 function rbv_kala_default_preprocess_node(&$variables) {
     if ($variables['view_mode'] != 'teaser') {
       $variables['classes_array']['class'][] = 'node-' . $variables['view_mode'];
   }
 }
+
+*/
